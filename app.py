f61533d8491024f9e95f09379a9687b26b253f1f
henry199101 <331243543@qq.com>
2018-12-19 18:25:34 +0800

    Add a view function search(), it can render base.html

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 # coding:utf-8
 
-from flask import Flask
+
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/home')
 def index():
     return '<h1>Hello, Knowledge Graph!</h1>'
+
+'''
+Search
+Introduction
+Contributors
+'''
+@app.route('/search')
+def search():
+    return render_template('base.html')
