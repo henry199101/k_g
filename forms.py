5ba07d3a9bb82977406b141017406ea037210c8b
henry199101 <331243543@qq.com>
2018-12-19 23:13:29 +0800

    先构建 1 个下拉列表的字段，因知识有限，暂时不在模板中完成这个下拉列表功能。

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 
 
 class SearchForm(FlaskForm):
     search_content = StringField('Search_content')
     search_button = SubmitField('Search it !')
+    pull_down_list = SelectField(u'Program', choices=[('py', 'python'), ('text', 'plain text')])
