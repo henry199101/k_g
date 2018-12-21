f0483e2333713bfe20b122e6a60456e38e5bf85f
henry199101 <331243543@qq.com>
2018-12-21 15:07:37 +0800

    Too many changes.

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 # coding:utf-8
 
-
-from flask import Flask, render_template
-from forms import SearchForm
+from flask import Flask, redirect, url_for, render_template, flash
+from forms import SearchForm, DeleteKnowledgeForm, NewKnowledgeForm
 from flask_sqlalchemy import SQLAlchemy
 import os, click
-
 
 app = Flask(__name__)
 app.secret_key = 'secret string'
@@ -15,20 +13,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(app.root_path, 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-
 @app.route('/')
-@app.route('/home')
-@app.route('/home/')
-@app.route('/index')
-@app.route('/index')
 def index():
-    return render_template('base.html')
+    return redirect(url_for('search'))
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
     form = SearchForm()
     return render_template('search.html', form=form)
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    form = SearchForm()
+    knowledges = KnowledgeTable.query.filter_by(entity='Li He')
+    return render_template('result.html', knowledges=knowledges, form=form)
+
 
 @app.route('/introduction')
 def introduction():
@@ -38,12 +37,29 @@ def introduction():
 def contributors():
     return render_template('contributors.html')
 
-class KnowledgeTable(db.Model):
-    entity      = db.Column(db.Text, name='entity', primary_key=True)
-    attr_name   = db.Column(db.Text, name='attr_name')
-    attr_value  = db.Column(db.Text, name='attr_value')
+@app.route('/new', methods=['GET', 'POST'])
+def new_knowledge():
+    form = NewKnowledgeForm()
+    if form.validate_on_submit():
+        entity      = form.entity.data
+        attr_name   = form.attr_name.data
+        attr_value  = form.attr_value.data
+        knowledge   = KnowledgeTable(entity=entity, attr_name=attr_name, attr_value=attr_value)
+        db.session.add(knowledge)
+        db.session.commit()
+        flash('A knowledge has been uploaded!')
+        return redirect(url_for('index'))
+    return render_template('admin.html', form=form)
 
 @app.cli.command()
 def initdb():
     db.create_all()
     click.echo('数据库初始化完毕！')
+
+class KnowledgeTable(db.Model):
+    entity      = db.Column(db.Text, name='entity', primary_key=True)
+    attr_name   = db.Column(db.Text, name='attr_name')
+    attr_value  = db.Column(db.Text, name='attr_value')
+
+    def __repr__(self):
+        return self.__class__.__name__ + '\'s entity: %r' % self.entity
