6132de271ba684a1e1f348310bf9f4f5e1aa5e44
henry199101 <331243543@qq.com>
2018-12-21 15:08:39 +0800

    Hard to describe the changes.

--- forms.py
+++ forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Length
 
 
 class SearchForm(FlaskForm):
-    search_content = StringField('Search_content')
-    search_button = SubmitField('Search it !')
-    pull_down_list = SelectField(u'Program', choices=[('py', 'python'), ('text', 'plain text')])
+    search_content  = StringField(label='below is a search kuang.')
+    search_button   = SubmitField('Search it !')
+    #pull_down_list = SelectField(u'Program', choices=[('py', 'python'), ('text', 'plain text')])
+
+
+class DeleteKnowledgeForm(FlaskForm):
+    submit = SubmitField('Delete')
+
+
+class NewKnowledgeForm(FlaskForm):
+    entity      = StringField('a entity')
+    attr_name   = StringField('an attr_name')
+    attr_value  = StringField('an attr_value')
+    submit      = SubmitField('Upload Knowledge!')
